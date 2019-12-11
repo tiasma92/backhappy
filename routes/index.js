6e8f6095aadce03f7c608436d9af735dff5f4c51
Mattias AZOULAY <mattias.azoualy@gmail.com>
2019-12-11 15:57:52 +0100

    connexion bdd

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/signin', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/sign-in',async function(req, res, next) {
+  console.log(req.query.email)
+  const user = await userModel.findOne({
+    email: req.query.email
+  })
+  if(user){
+    console.log('We found a User with this email')
+    res.json({user});
+  }else{
+      console.log('There is no user with this email ! So we need to add the user')
+      console.log(user)
+    res.json({result: false})
+  }});
 
 router.post('/sign-up',async function(req, res, next) {
   const user = await userModel.findOne({
